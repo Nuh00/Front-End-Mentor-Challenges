7c81f06be7f32b5a22a22b7ea828906e496a971a
Noah Barkat <146979589+Nuh00@users.noreply.github.com>
2024-03-06 12:10:19 -0600

    I made a FAQ page that has a little bit of js functionality I changes the image when clicked on and also shows/hides the content of the question

--- FAQ/script.js
+++ FAQ/script.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+let buttonImg = document.querySelectorAll('#buttonImg');
+
+
+buttonImg.forEach((button) => {
+    button.addEventListener('click', (renderButton) )
+}); 
+
+
+
+function renderButton(event) {
+    const parent = event.target.closest('.accordian-item');
+    const content = parent.querySelector('.accordian-item-content');
+    const img = parent.querySelector('img');
+    img.src = img.src.includes('plus') ? 'images/icon-minus.svg' : 'images/icon-plus.svg';
+    content.classList.toggle('show');
+
+    
+}
+
+
+// todo 1: obtain the class to "show" and remove "show"
+// todo 2: obtain the image so it can be changed
