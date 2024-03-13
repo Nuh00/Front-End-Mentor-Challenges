b62aac215f61c774ee7e0b11e879bcba22f4628c
Noah Barkat <146979589+Nuh00@users.noreply.github.com>
2024-03-13 18:57:21 -0500

    update

--- pricingCards/script.js
+++ pricingCards/script.js
@@ -1,1 +1,12 @@
-console.log('script.js');
+const sliderInput = document.querySelector('#toggle');
+const numbers = document.querySelectorAll('#num');
+sliderInput.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        numbers.forEach((number) => {
+        
+        });
+    }
+
+});
+
+
